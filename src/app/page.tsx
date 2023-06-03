30ba4a34f7dd677cae73cff271465fee656e4c5d
Nicolas L. Kaminski <nicolas.kaminski.2001@gmail.com>
2023-06-03 18:09:03 -0300

    Add Leaflet React

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+
 export default function Home() {
   return (
     <>
-      <main className="flex p-3 items-center justify-center">Hello World!</main>
+      <main className="flex p-3 items-center justify-center">
+        Hello World!
+        <MapContainer></MapContainer>
+      </main>
     </>
   );
 }
